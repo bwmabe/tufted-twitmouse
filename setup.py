f4f77685cc63cbda191e103ae3a659a06f9c99ff
bwmabe <9590854+bwmabe@users.noreply.github.com>
2021-04-07 19:31:18 -0400

    bumped version number for build testing

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="tufted_twitmouse",
-    version="0.0.1",
+    version="0.0.2",
     author="bwmabe",
     author_email="benwmabe@gmail.com",
     description="A wrapper around Tweepy for use with other projects",
