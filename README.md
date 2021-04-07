691e0371335f846c3dd5e735ae426b795bbf58ca
bwmabe <9590854+bwmabe@users.noreply.github.com>
2021-04-07 10:39:32 -0400

    Initial Commit
    * Added base structure of a Python package as according to the docs
    
    * Skeleton package
    
    * Skeleton readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tufted Twitmouse
+
+A library to read a stream of Tweets asynchronously and send the information to another process.
+
+## Dependancies
+
+## Other Stuff
+
+## Issues
+
+(c) 2012 bwmabe
---
+++ setup.py
@@ -0,0 +1,27 @@
+# Adapted from https://packaging.python.org/tutorials/packaging-projects/
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tufted_twitmouse",
+    version="0.0.1",
+    author="bwmabe",
+    author_email="benwmabe@gmail.com",
+    description="A twitter stream reader library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bwmabe/tufted-twitmouse",
+    project_urls={
+        "Bug Tracker": "https://github.com/bwmabe/tufted-twitmouse/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
