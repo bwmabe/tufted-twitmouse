0b128658bf731880fb5d2bbb1f8cf9e21400c5f7
bwmabe <9590854+bwmabe@users.noreply.github.com>
2021-04-17 17:39:08 -0400

    fixed some bugs and typos

--- example.py
+++ example.py
@@ -3,9 +3,9 @@ from ttwitmouse import TTwitmouse
 
 
 async def main():
-    ttmouse = TTwitmouse('twitter_api_key', '3160892443')
+    ttmouse = TTwitmouse('twitter_api_key', ID_TO_FOLLOW)
     queue = asyncio.Queue()
-    tweet_getter = asyncio.create_task(ttmouse.listen_for("#GTAOnline", queue, interval=30))
+    tweet_getter = asyncio.create_task(ttmouse.listen_for(TERM_TO_SEARCH, queue, interval=30))
     while True:
         tweets = await queue.get()
         for tweet in tweets:
